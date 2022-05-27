f5e8fded4ec70a00fb250d2e431476aa64515878
Levy Barbosa <marcio.levy20@gmail.com>
2022-05-27 20:45:02 -0300

    fifo implementation
    Co-authored-by: Gustavo Lopes <pstarofoli@gmail.com>

---
+++ src/fifo.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+struct Page {
+    number: u32,
+}
+
+type Frames = Vec<Option<Page>>;
+
+fn get_page(frames: &Frames, page_number: u32) -> Option<&Page> {
+    for frame in frames {
+        if let Some(page) = frame {
+            if page.number == page_number {
+                return Some(page);
+            }
+        }
+    }
+    return None;
+}
+
+fn insert_page(frames: &mut Frames, page_number: u32) {
+    frames.pop();
+    frames.insert(
+        0,
+        Some(Page {
+            number: page_number,
+        }),
+    );
+}
+
+pub fn simulate(refs: &Vec<u32>, number_of_frames: usize) -> u32 {
+    let mut frames: Frames = (0..number_of_frames).map(|_| None).collect();
+    let mut page_faults: u32 = 0;
+
+    for reference in refs {
+        let page = get_page(&frames, *reference);
+
+        if page.is_none() {
+            page_faults += 1;
+            insert_page(&mut frames, *reference);
+        }
+    }
+
+    return page_faults;
+}
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use rand::{distributions::Uniform, Rng};
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+mod fifo;
+
+fn create_references(number_of_references: usize, number_of_pages: u32) -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+
+    // Range for the random number generator
+    let range = Uniform::<u32>::new(0, number_of_pages);
+
+    return (0..number_of_references)
+        .map(|_| rng.sample(&range))
+        .collect();
+}
+
+fn write_refs_to_file(path: String, refs: &Vec<u32>, number_of_pages: u32) {
+    let mut file = File::create(&path).unwrap();
+
+    let header = format!("{} {}\n", refs.len(), number_of_pages);
+    file.write_all(header.as_bytes()).unwrap();
+
+    for reference in refs {
+        let page = format!("{}\n", reference);
+        file.write_all(page.as_bytes()).unwrap();
+    }
+
+    println!("References written to {}", path);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 5 {
+        println!(
+            "Usage: {} <number_of_frames> <number_of_pages> <number_of_references> <path_to_save_refs_file>\n",
+            args[0]
+        );
+        return;
+    }
+
+    let number_of_frames: usize = args[1]
+        .parse()
+        .expect("Invalid number of frames (1st argument)");
+    let number_of_pages: u32 = args[2]
+        .parse()
+        .expect("Invalid number of pages (2nd argument)");
+    let number_of_references: usize = args[3]
+        .parse()
+        .expect("Invalid number of references (3rd argument)");
+    let path_refs_file = (&args[4]).to_string();
+
+    let refs = create_references(number_of_references, number_of_pages);
+
+    write_refs_to_file(path_refs_file, &refs, number_of_pages);
+
+    let fifo_page_faults = fifo::simulate(&refs, number_of_frames);
+
+    println!("FIFO page faults: {}", fifo_page_faults);
+}
